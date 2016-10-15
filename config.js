9046268259f6a3c4d46281c32903101dafff79c4
Dmytro Lyman <liman.dimitriy@gmail.com>
2016-10-15 22:43:10 +0300

    possibly better config

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-const params = { betta0: 1.5,
+const params = { betta0: 1.2,
                  gamma: 1.0,
                  lambdaMin: 0.0,
                  lambdaMax: 1.0,
                  alpha0: 0.5,
-                 totalIterations: 1000,
+                 totalIterations: 2000,
                  fireFliesAmount: 500,
-                 dimension: 2
+                 dimension: 3
                };
 
 module.exports = params;
