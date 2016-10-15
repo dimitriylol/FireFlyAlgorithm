2c71aa9da11f87e08b1189248a3c75fc2d515bdc
Dmytro Lyman <liman.dimitriy@gmail.com>
2016-10-15 22:42:30 +0300

    make visualization via web

--- main.js
+++ main.js
@@ -1,12 +1,72 @@
+const http = require('http');
 const minFireFly = require('./FireFlyAlg/minFireFly.js');
-const params = require('./config.js');
 const funcs = require('./findExtremumOnFunc.js');
 const visualizeData = require('./visualize/visualize.js');
+const readFile = require('fs').readFile;
+const normalizePath = require('path').normalize;
 
-funcs
-    .forEach(({ lambda, funcName, start, end }) =>
-             visualizeData(funcName,
-                           minFireFly(Object.assign(params,
-                                                    { startPosition: start,
-                                                      endPosition: end }),
-                                      lambda)));
+let params = require('./config.js');
+
+function readFilePromise (pathToFile) {
+    return when.promise((resolve, reject) =>
+                        readFile(normalizePath(pathToFile),
+                                 (err, data) => {
+                                     if (err) reject(err);
+                                     resolve(data);
+                                 }));
+}
+
+function min (arrFireFlies) {    
+    return arrFireFlies.map((fireFly) => fireFly.rank).sort()[0];
+}
+
+function flyFireFlies (fireFliesAmount, lambda, params) {
+    const iterationOfFlies = 10;
+    const averageMinFireFly = min(Array.from(new Array(iterationOfFlies),
+                                             () => minFireFly(params, lambda)));
+    return { fireFliesAmount,
+             minFireFly
+           };
+}
+
+function visualizeFireFliesRes () {
+    const rangeFireFliesAmount = Array.from(new Array(10), (_, index) => index*100 + 200);
+    return visualizeData(
+        funcs
+            .map(({ lambda, funcName, start, end }) =>
+                 ({ funcName,
+                    arrBestFireFlies: (rangeFireFliesAmount                                     
+                                       .map((fireFliesAmount) =>
+                                            flyFireFlies(fireFliesAmount,
+                                                         lambda,
+                                                         Object
+                                                         .assign(params,
+                                                                 { fireFliesAmount,
+                                                                   startPosition: start,
+                                                                   endPosition: end
+                                                                 }))))
+                  })));
+}
+
+const server = http.createServer((req, res) => {
+    try {    
+        res.writeHead(200, {'Access-Control-Allow-Origin': '*',
+                            'Content-Type': 'application/json'});
+        res.end(visualizeFireFliesRes());
+        console.log('all successfully done');
+    }
+    catch (err) {
+        console.log('smth wrong', err);
+        res.writeHead(500, {'Access-Control-Allow-Origin': '*',
+                            'Content-Type': 'text/plain'});
+        res.end('There is error ' + err.toString());
+    }
+});
+
+server.on('clientError', (err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+
+server.listen(8000);
+
+
--- visualize/visualize.js
+++ visualize/visualize.js
@@ -1,5 +1,32 @@
-function visualizeData (funcName, bestFly) {
-    console.log(`best firefly for ${funcName} is ${bestFly}`);
+const toCSV = require('./toCSV.js');
+const generateGraphicFromCSV = require('./graphicFromCSV.js');
+const resolvePath = require('path').resolve;
+const when = require('when');
+
+/**
+ * arrFireFliesRes is array of object
+ * { [String] funcName,
+ *   [Array] arrBestFireFlies }
+ */
+function visualizeData (arrFireFliesRes) {
+    return JSON.stringify(arrFireFliesRes
+                          .map(({ funcName, arrBestFireFlies }) =>
+                               ({ funcName,
+                                  graphic: { axisX: 'fireFliesAmount',
+                                             axisY: 'minFireFly',
+                                             values: arrBestFireFlies
+                                           }
+                                })));
 }
 
 module.exports = visualizeData;
+
+
+
+
+
+
+
+
+
+
